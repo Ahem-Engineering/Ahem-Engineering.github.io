76dea8c8e0534272e815c68d61bfa2d7d9b104b7
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-03 15:55:16 +1000

    Update populate.js

--- script/populate.js
+++ script/populate.js
@@ -1,5 +1,6 @@
-function(data) {
+function populateDiv(data, div) {
   var git_data = JSON.parse(data);
   console.log("Data returned from Git server:");
   console.log(git_data);
+  div.innerHTML = "<code>" + data + "</code>";
 }

b5245c9baf4b49eb4d7eaf33499ffdf8e814238e
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-03 17:05:16 +1000

    Update populate.js

--- script/populate.js
+++ script/populate.js
@@ -2,6 +2,8 @@
 
 
 function populateDivById(div_id, data) {
+  console.log('populating ' + div_id + ' using:');
+  console.log(data);
   document.getElementById(div_id).innerHTML = 
     '<div><b>datasheet:</b>&nbsp;<a href="' + data.datasheet[0].url + '">' + data.datasheet[0].author + "</a></div>" +
     '<div><b>library:</b>&nbsp;<a href="' + data.library[0].url + '">' + data.library[0].author + "</a></div>";

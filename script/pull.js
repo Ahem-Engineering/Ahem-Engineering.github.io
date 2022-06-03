600339414cd7cbb0f64df6a63875f37f97ba1999
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-03 17:00:44 +1000

    Update pull.js

--- script/pull.js
+++ script/pull.js
@@ -1,3 +1,7 @@
+// Call by passing URL to pull JSON data from and function to execute using the data
+//  url - URL in text format
+//  fn - function with a single parameter which is a JSON object collected from the URL
+
 function pullJSON(url, fn) {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {

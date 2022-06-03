509c15e0209d573a35f03daaac730d8f4c8e01a4
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-03 21:10:35 +1000

    Update pull.js

--- script/pull.js
+++ script/pull.js
@@ -2,11 +2,11 @@
 //  url - URL in text format
 //  fn - function with a single parameter which is a JSON object collected from the URL
 
-var git_data = {};
+var git_data_this = {};
 function pullJSON(url, fn) {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-    git_data = this.responseText;
+    git_data_this = this;
     //fn(git_data);
     fn(JSON.parse(this.responseText));
   };

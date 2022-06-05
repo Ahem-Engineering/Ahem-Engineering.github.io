358e049a78c8a2281565c16b68586f7c3fc1686c
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-05 15:20:38 +1000

    Update pull.js

--- script/pull.js
+++ script/pull.js
@@ -3,12 +3,16 @@
 //  fn - function with a single parameter which is a JSON object collected from the URL
 
 var git_data_this = {};
-function pullJSON(url, fn) {
+function pullJSON(url, on200, on404 = null) {
   var request = new XMLHttpRequest();
   request.onloadend = function() {
-    if (this.status != 200) return;
-    var data = JSON.parse(this.responseText);
-    fn(data);
+    if (this.status == 404) on404();
+    else if (this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      on200(data);
+    } else {
+      console.log("error: server returned " + this.responseText);
+    }
   };
   request.open('get', url, true);
   request.send();

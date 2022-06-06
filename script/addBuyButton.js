ca4ed05e9093e223ab04969e5b535d0680df3e4d
Ahem Engineering <106718318+Ahem-Engineering@users.noreply.github.com>
2022-06-06 13:06:26 +1000

    Update addBuyButton.js

--- script/addBuyButton.js
+++ script/addBuyButton.js
@@ -1,15 +1,18 @@
 function addBuyButtonToId(html_id, product_id) {
+  console.log("adding buy button (" + product_id + ") to " + html_id);
   pullJSON (
     'https://git.ahem.net.au/shopify-buy-button-options.json',
     function (options) {  
+      console.log("creating client");
       var client = ShopifyBuy.buildClient(
         {
           domain: 'ahem-engineering.myshopify.com',
           storefrontAccessToken: 'c43dd5285b9eaf2ce09081222ee4c393',
         }
       );
+      console.log("adding component");
       ShopifyBuy.UI.onReady(client).then(
-        function (ui) {
+        function createComponentWithUi(ui) {
           ui.createComponent(
             'product', 
             {
